706b8d78ca8c14fd83cf50d9e5244928c74a716d
DanielGCh1 <dgchaarturo@gamil.com>
2023-01-13 09:51:42 -0600

    Correcion de algunos errores

--- src/App.js
+++ src/App.js
@@ -18,40 +18,62 @@ import { EmployeeProvider } from './context/Employee/EmployeeContext';
 import { PromotionProvider } from './context/Promotion/PromotionContext';
 import CreacionPromociones from './routes/CreacionPromociones';
 
+import LoginCliente from './routes/loginCliente';
+import RegistrarCliente from "./routes/registrarCliente";
+import Nosotros from "./routes/nosotros";
+import Contacto from "./routes/contacto";
+import BuscarCliente from './routes/buscarCliente';
+import IngredientContext, { IngredientProvider } from './context/Ingredients/IngredientContext';
+import { ClienteProvider } from './context/Cliente/ClienteContext';
+import { ProductosValoresProvider } from './context/Product/ProductosValoresContext';
+
 function App() {
   return (
     <ChakraProvider theme={theme}>
         <ProductProvider>
-          <EmployeeProvider>
-            <PromotionProvider>
-            <Routes>
-              <Route path="/" element={<Login />} />
-              <Route path="Register" element={<Register />} />
-              <Route path="/Home" element={<Home />}>
-                <Route path="RegisterMaterial" element={<RegisterMaterial />} />
-                <Route path="BuscarMaterial" element={<BuscarMaterial />} />
-                <Route
-                  path="CreacionProductos"
-                  element={<CreacionProductos />}
-                />
-                <Route
-                  path="CreacionEmpleados"
-                  element={<CreacionEmpleados />}
-                />
-                <Route
-                  path="CreacionPromociones"
-                  element={<CreacionPromociones />}
-                />
-              </Route>
-              <Route path="/PaginaPrincipal" element={<PaginaPrincipal />} />
-              <Route
-                path="ProductoVentaPedido"
-                element={<ProductoVentaPedido />}
-              />
-              <Route path="PerfilUsuario" element={<PerfilUsuario />} />
-            </Routes>
-            </PromotionProvider>
-          </EmployeeProvider>
+          <ProductosValoresProvider>
+            <ClienteProvider>
+              <IngredientProvider>
+                <EmployeeProvider>
+                  <PromotionProvider>
+                  <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="Register" element={<Register />} />
+                    <Route path="/Home" element={<Home />}>
+                      <Route path="RegisterMaterial" element={<RegisterMaterial />} />
+                      <Route path="BuscarMaterial" element={<BuscarMaterial />} />
+                      <Route
+                        path="CreacionProductos"
+                        element={<CreacionProductos />}
+                      />
+                      <Route
+                        path="CreacionEmpleados"
+                        element={<CreacionEmpleados />}
+                      />
+                      <Route
+                        path="CreacionPromociones"
+                        element={<CreacionPromociones />}
+                      />
+                      <Route path="BuscarCliente" element={<BuscarCliente />} />
+
+                    </Route>
+                    <Route path="/PaginaPrincipal" element={<PaginaPrincipal />} />
+                    <Route
+                      path="ProductoVentaPedido"
+                      element={<ProductoVentaPedido />}
+                    />
+                    <Route path="/LoginCliente" element={<LoginCliente />} />
+                    <Route path="PerfilUsuario" element={<PerfilUsuario />} />
+                    <Route path="RegistrarCliente" element={<RegistrarCliente />} />
+                    <Route path="Nosotros" element={<Nosotros />} />
+                    <Route path="Contacto" element={<Contacto />} />
+                  
+                  </Routes>
+                  </PromotionProvider>
+                </EmployeeProvider>
+              </IngredientProvider>
+            </ClienteProvider>
+          </ProductosValoresProvider>
         </ProductProvider>
     </ChakraProvider>
   );
--- src/routes/paginaPrincipal.js
+++ src/routes/paginaPrincipal.js
@@ -44,9 +44,9 @@ export default function paginaPrincipal() {
                 </Stack>
 
                 <AspectRatio ratio={16 / 9}>
-                    <iframe
-                        src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.952912260219!2d3.375295414770757!3d6.5276316452784755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8b2ae68280c1%3A0xdc9e87a367c3d9cb!2sLagos!5e0!3m2!1sen!2sng!4v1567723392506!5m2!1sen!2sng'
-                    />
+
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3936.5903856472532!2d-83.68107094127808!3d9.369467042815717!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa14fcec96d1653%3A0xaabcb7b8d5b0b0dd!2zUGFsIGNhZsOp!5e0!3m2!1ses!2scr!4v1673391758308!5m2!1ses!2scr"></iframe>
+
                 </AspectRatio>
 
                 <Flex
